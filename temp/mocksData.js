a9524a0f6ad65811574b3541a75a324904c8a599
Goryachenok <goryachev.na72@gmail.com>
2022-05-30 13:36:44 +0500

    ready_work

---
+++ temp/mocksData.js
@@ -0,0 +1,47 @@
+'head': {
+    defaults: {
+
+    }
+}
+
+
+
+
+,
+
+__iconsData: {
+    
+        'close': {
+            width: '12px',
+            height: '12px'
+        },
+    
+        'phone': {
+            width: '16px',
+            height: '16px'
+        },
+    
+        'up': {
+            width: '13px',
+            height: '15px'
+        },
+    
+},
+
+__pages: [{
+                name: '!main',
+                href: '!main.html',
+                desc: 'Главная',
+             },{
+                name: '404',
+                href: '404.html',
+                desc: '404',
+             },{
+                name: 'index',
+                href: 'index.html',
+                desc: '//',
+             },{
+                name: 'page',
+                href: 'page.html',
+                desc: 'Текстовая страница',
+             }]
---
+++ temp/symbols-data-template.js
@@ -0,0 +1,18 @@
+__iconsData: {
+    
+        'close': {
+            width: '12px',
+            height: '12px'
+        },
+    
+        'phone': {
+            width: '16px',
+            height: '16px'
+        },
+    
+        'up': {
+            width: '13px',
+            height: '15px'
+        },
+    
+}
